9b06a6e607eff1449d81bd21f4b8987185b7d3a3
Svitlana Shumal <svetlankachajka@gmail.com>
2025-10-31 13:19:06 +0200

    fixed code

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import imagemin from "imagemin";
-import imageminWebp from "imagemin-webp";
-import path from "path";
-import { defineConfig } from "vite";
-import glob from "fast-glob";
-import { fileURLToPath } from "url";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-import purgecss from "@fullhuman/postcss-purgecss";
+import imagemin from 'imagemin';
+import imageminWebp from 'imagemin-webp';
+import path from 'path';
+import { defineConfig } from 'vite';
+import glob from 'fast-glob';
+import { fileURLToPath } from 'url';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import purgecss from '@fullhuman/postcss-purgecss';
 
 export default defineConfig({
+  base: './',
   plugins: [
     ViteImageOptimizer({
       png: {
@@ -21,14 +22,14 @@ export default defineConfig({
       },
     }),
     {
-      ...imagemin(["./src/img/**/*.{jpg,png,jpeg}"], {
-        destination: "./src/img/webp/",
+      ...imagemin(['./src/img/**/*.{jpg,png,jpeg}'], {
+        destination: './src/img/webp/',
         plugins: [imageminWebp({ quality: 86 })],
       }),
-      apply: "serve",
+      apply: 'serve',
     },
     purgecss({
-      content: ["./**/*.html"],
+      content: ['./**/*.html'],
     }),
   ],
   build: {
@@ -36,8 +37,8 @@ export default defineConfig({
     rollupOptions: {
       input: Object.fromEntries(
         glob
-          .sync(["./*.html", "./pages/**/*.html"])
-          .map((file) => [
+          .sync(['./*.html', './pages/**/*.html'])
+          .map(file => [
             path.relative(
               __dirname,
               file.slice(0, file.length - path.extname(file).length)
@@ -47,7 +48,7 @@ export default defineConfig({
       ),
       // output unminified CSS file
       output: {
-        assetFileNames: "assets/[name].[ext]",
+        assetFileNames: 'assets/[name].[ext]',
       },
     },
   },
